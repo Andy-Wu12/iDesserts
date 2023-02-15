b7daa793b3e51862fb5960d948da378ecd364d06
Andy Wu <awpats12@gmail.com>
2023-02-14 20:08:32 -0500

    Initial commit

---
+++ iDesserts/iDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDessertsApp.swift
+//  iDesserts
+//
+//  Created by Andy Wu on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct iDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
